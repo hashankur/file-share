6047f01b126440b54dd7e6f3e7c1a3c309f28171
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2025-06-13 22:54:58 +0530

    feat: add styles

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,23 @@ import path from "path";
 import { networkInterfaces } from "os";
 
 type File = {
-  folder: string;
+  folder: string | null;
   filename: string;
 };
 
 const PORT = 8081;
 const FILE_DIRECTORY = "/public";
 const TITLE = "File Share";
+
+Bun.spawn({
+  cmd: [
+    "tailwindcss",
+    "-i",
+    "./src/assets/main.css",
+    "-o",
+    "./public/output.css",
+  ],
+});
 
 // Get IPv4 network addresses (non-internal)
 function address() {
@@ -37,8 +47,9 @@ function traverse(dir: string, files: File[]) {
     if (fs.statSync(fullPath).isDirectory()) {
       traverse(fullPath, files);
     } else {
+      let folderName = dir.replace(FILE_DIRECTORY, "");
       files.push({
-        folder: dir.replace(FILE_DIRECTORY, ""),
+        folder: folderName === "" ? null : folderName,
         filename: file,
       });
     }
@@ -46,14 +57,15 @@ function traverse(dir: string, files: File[]) {
 }
 
 function groupByFolderName(files: File[]) {
-  return Object.groupBy(files, ({ folder }) => folder);
+  return Object.groupBy(files, ({ folder }) => folder ?? "");
 }
 
 const app = new Elysia()
   .use(html())
+  .use(staticPlugin({ prefix: "/", noCache: true }))
   .use(
     staticPlugin({
-      prefix: "/", // Serve static files from the root
+      prefix: "/assets",
       assets: FILE_DIRECTORY,
       enableDecodeURI: true,
     }),
@@ -66,28 +78,37 @@ const app = new Elysia()
     return (
       <html lang="en">
         <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="stylesheet" href="output.css" />
           <title>{TITLE}</title>
         </head>
-        <body style={{ backgroundColor: "#0b0e14", color: "#bfbfb6" }}>
-          <h1>{TITLE}</h1>
+        <body class="bg-neutral-950 text-neutral-400 container mx-auto my-10 px-5 break-all">
+          <h1 class="text-4xl font-bold">{TITLE}</h1>
           {Object.keys(payload)
             .sort((a, b) => new Intl.Collator().compare(a, b))
             .map((folder) => (
               <>
-                <h2 style={{ marginTop: "2em" }}>
-                  {folder === "" ? "/ (Index)" : folder}
+                <h2 class="text-2xl font-bold mt-8 my-5">
+                  {folder ? folder : "/"}
                 </h2>
-                {payload[folder]
-                  ?.sort((a, b) =>
-                    new Intl.Collator().compare(a.filename, b.filename),
-                  )
-                  .map((file) => (
-                    <p>
-                      <a href={`${file.folder}/${file.filename}`}>
+                <div class="flex flex-col divide-y-1 divide-neutral-800">
+                  {payload[folder]
+                    ?.sort((a, b) =>
+                      new Intl.Collator().compare(a.filename, b.filename),
+                    )
+                    .map((file) => (
+                      <a
+                        class="text-blue-400 p-3 hover:bg-neutral-400 hover:text-neutral-950 visited:text-purple-400"
+                        href={`assets/${file.folder ? file.folder + "/" : ""}${file.filename}`}
+                      >
                         {file.filename}
                       </a>
-                    </p>
-                  ))}
+                    ))}
+                </div>
               </>
             ))}
         </body>
