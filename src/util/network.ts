42f18db6015b7df82adaeba10f4b0ffe7803db69
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2025-06-27 22:09:03 +0530

    fix: select from non-virtual network interfaces

--- src/util/network.ts
+++ src/util/network.ts
@@ -3,16 +3,41 @@ import { networkInterfaces } from "os";
 // Get IPv4 network addresses (non-internal)
 export function address() {
   const nets = networkInterfaces();
-  const results = Object.create(null);
+  const virtualInterfacePrefixes = [
+    "tailscale",
+    "ts", // Common Tailscale alias
+    "docker",
+    "br-", // Bridge interfaces
+    "veth", // Virtual Ethernet interfaces
+    "utun", // macOS VPN/virtual interfaces (often Tailscale)
+    "ppp", // Point-to-Point Protocol (modems, some VPNs)
+    "tun", // Tunnel interfaces
+    "tap", // Tap interfaces
+    "wsl", // WSL specific interfaces
+  ];
+
+  let foundAddress: string | undefined;
 
   for (const name of Object.keys(nets)) {
-    if (nets[name] === undefined) return "localhost";
-    for (const net of nets[name]) {
-      if (net.family === "IPv4" && !net.internal) {
-        if (!results[name]) results[name] = [];
-        results[name].push(net.address);
+    const netInterfaces = nets[name];
+    if (netInterfaces === undefined) continue;
+
+    const isVirtual = virtualInterfacePrefixes.some((prefix) =>
+      name.startsWith(prefix),
+    );
+
+    for (const net of netInterfaces) {
+      // Skip over non-IPv4, internal (i.e. 127.0.0.1), and virtual addresses
+      if (net.family === "IPv4" && !net.internal && !isVirtual) {
+        foundAddress = net.address;
+        break;
       }
     }
+    if (foundAddress) {
+      break;
+    }
   }
-  return Object.values(results)[0];
+
+  // Return the found address or default to localhost
+  return foundAddress || "localhost";
 }
